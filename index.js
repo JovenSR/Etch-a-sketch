35cd791b1938014ca6ad6b59d5ff55413c72aa54
Tyson Rai <jovensrai@gmail.com>
2019-07-21 16:14:52 -0700

    Added css, logic to create 16x16 grid of divs, hovering changes color on divs

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-alert("Hellooo");
+for(let i = 0; i < 16; i++) {
+	for(let j = 0; j < 16; j++) {
+		let div = document.createElement('div');
+		div.classList.add("div");
+		document.querySelector('.container').append(div);
+	}
+}
+
+$(".div").hover(function(){
+  $(this).css("background-color", "yellow");
+  }, function(){
+  $(this).css("background-color", "pink");
+});
