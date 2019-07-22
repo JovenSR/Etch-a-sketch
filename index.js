5fe2640ae828b49cf4cfdc336fcacf1392e9ff00
Tyson Rai <jovensrai@gmail.com>
2019-07-21 20:42:02 -0700

    Can clear grid and create new grid

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+const button = document.querySelector('#button');
+button.addEventListener('click', (e) => {
+	let answer = prompt('Enter a number between 1 and 100');
+	const con = document.querySelector('.container');
+	document.querySelector('.container').innerHTML = " ";
+	for(let i = 0; i < answer; i++) {
+		for(let j = 0; j < answer; j++) {
+			let div = document.createElement('div');
+			div.classList.add("div");
+			con.append(div);
+		}
+	}
+
+	con.style.gridTemplateColumns = `repeat(${answer}, 1fr)`
+	con.style.gridTemplateRows = `repeat(${answer}, 1fr)` 
+
+	$(".div").hover(function(){
+		$(this).css("background-color", "yellow");
+	  	}, function(){
+	  	$(this).css("background-color", "pink"); 
+	});
+})
+
+
 for(let i = 0; i < 16; i++) {
 	for(let j = 0; j < 16; j++) {
 		let div = document.createElement('div');
@@ -9,5 +33,5 @@ for(let i = 0; i < 16; i++) {
 $(".div").hover(function(){
   $(this).css("background-color", "yellow");
   }, function(){
-  $(this).css("background-color", "pink");
+  $(this).css("background-color", "pink"); 
 });
