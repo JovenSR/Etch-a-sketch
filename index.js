5ce3b20bc4b49d3563d6f309c81c24c4c36fb666
Tyson Rai <jovensrai@gmail.com>
2019-07-22 19:36:38 -0700

    All logic completed as well as most styling

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
-const button = document.querySelector('#button');
-button.addEventListener('click', (e) => {
+const button1 = document.querySelector('#button1');
+const highlight = document.querySelector('#highlight');
+const button2 = document.querySelector('#button2');
+const gridColor = document.querySelector('#gridColor');
+
+var colorPicker1 = new iro.ColorPicker(highlight, {
+		color: '#f00'
+	});;
+
+var colorPicker2 = new iro.ColorPicker(gridColor, {
+		color: '#f00'
+	});;
+
+button1.addEventListener('click', (e) => {
+	 if(highlight.style.display === "none"){
+	 	highlight.style.display = "grid"
+	 } else {
+	 	highlight.style.display = "none"
+	 }
+	function onColorChange(color, changes) {
+		let pickedColor = color.hexString;
+ 		$(".div").hover(function(){
+  			$(this).css("background-color", pickedColor);
+  		}, function(){
+  			$(this).css("background-color", pickedColor); 
+		});
+	}
+	// listen to a color picker's color:change event
+	colorPicker1.on('color:change', onColorChange);	
+})
+
+button2.addEventListener('click', (e) => {
+	 if(gridColor.style.display === "none"){
+	 	gridColor.style.display = "grid"
+	 } else {
+	 	gridColor.style.display = "none"
+	 }
+	 function onColorChange(color, changes) {
+	 	let newColor = color.hexString;
+	 	$('.div').css('border', '1px solid' + newColor);
+	 	$('.container').css('outline', '1px solid' + newColor);
+	 }
+	 // listen to a color picker's color:change event
+	colorPicker2.on('color:change', onColorChange);	
+})
+
+
+const button3 = document.querySelector('#button3');
+button3.addEventListener('click', (e) => {
 	let answer = prompt('Enter a number between 1 and 100');
 	const con = document.querySelector('.container');
 	document.querySelector('.container').innerHTML = " ";
@@ -10,17 +57,14 @@ button.addEventListener('click', (e) => {
 			con.append(div);
 		}
 	}
-
 	con.style.gridTemplateColumns = `repeat(${answer}, 1fr)`
 	con.style.gridTemplateRows = `repeat(${answer}, 1fr)` 
-
 	$(".div").hover(function(){
-		$(this).css("background-color", "yellow");
+		$(this).css("background-color", "#E76C67");
 	  	}, function(){
-	  	$(this).css("background-color", "pink"); 
+	  	$(this).css("background-color", "#E76C67"); 
 	});
 })
-
 
 for(let i = 0; i < 16; i++) {
 	for(let j = 0; j < 16; j++) {
@@ -31,7 +75,7 @@ for(let i = 0; i < 16; i++) {
 }
 
 $(".div").hover(function(){
-  $(this).css("background-color", "yellow");
+  $(this).css("background-color", "#E76C67");
   }, function(){
-  $(this).css("background-color", "pink"); 
+  $(this).css("background-color", "#E76C67"); 
 });
